bc42168d68904e1b185ffbb2daf3aa1ca3fd3abe
mmtakeuchi <mikemtaki@gmail.com>
2020-08-06 10:16:22 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
+
+  accepts_nested_attributes_for :comments, reject_if: proc { |comments| comments['content'].blank? }
 end
