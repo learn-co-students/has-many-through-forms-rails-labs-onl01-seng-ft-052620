4af84a1c0bcc199c44dcea667764673de24fe7fb
Jacques_Potgieter <Potgieter.g.jacques@gmail.com>
2020-08-07 10:33:59 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @commenters = []
+    @post.comments.each do |comment|
+      @commenters << comment.user if !@commenters.include?(comment.user)
+
+    end
   end
 
   def index
@@ -19,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
+    params.require(:post).permit(:title, :content, category_ids: [], categories_attributes: [:name], comment_content: [])
   end
 end
