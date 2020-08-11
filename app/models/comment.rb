525e8817760ebf2cc3895cf4b427d46456b544a3
Maloochow <luyao.mly@gmail.com>
2020-08-11 10:44:43 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
+
+  before_validation :find_or_create_user
+
+  def find_or_create_user
+    if user = User.find_by(username: self.user.username)
+      self.user = user
+    end
+  end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,24 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  accepts_nested_attributes_for :categories, reject_if: proc { |attr| attr['name'].blank? }
+  before_validation :find_or_create_category
+
+  def find_or_create_category
+    self.categories = self.categories.map do |category|
+      Category.find_or_create_by(name: category.name)
+    end
+  end
+
+  def comment_contents(user)
+    comments = self.comments.where(user: user).map {|comment| comment.content}
+    comments.join("; ")
+  end
+
+  def category_names
+    comments = self.categories.map {|category| category.name}
+    comments.join(", ")
+  end
+
 
 end
